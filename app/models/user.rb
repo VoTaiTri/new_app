c0a473183200caa29d6aa7577a6615eff04dec64
VoTaiTri <vo.tai.tri@framgia.com>
2015-01-22 22:39:43 +0700

    update log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,12 @@ class User < ActiveRecord::Base
 
 
 	# Returns the hash digest of the given string.
-	  	def User.digest(string)
-	    	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	def User.digest(string)
+	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 	                                                  BCrypt::Engine.cost
-	    	BCrypt::Password.create(string, cost: cost)
-	  	end
+	    BCrypt::Password.create(string, cost: cost)
 	end
+	
 
 	# Returns a random token.
   	def User.new_token
@@ -45,3 +45,5 @@ class User < ActiveRecord::Base
   	def forget
     	update_attribute(:remember_digest, nil)
   	end
+
+end
