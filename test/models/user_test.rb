4c85fb0906016b223ac3025e427bdd47b182f581
VoTaiTri <vo.tai.tri@framgia.com>
2015-01-22 22:13:09 +0700

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 	def setup
     	@user = User.new(name: "Example User", email: "user@example.com",
-                     	password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   	end
 
 	test "should be valid" do
@@ -50,5 +50,7 @@ class UserTest < ActiveSupport::TestCase
 	    assert_not @user.valid?
 	end
 
-
+	test "authenticated? should return false for a user with nil digest" do
+    	assert_not @user.authenticated?('')
+  	end
 end
